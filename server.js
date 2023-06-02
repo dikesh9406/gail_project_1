33720edc52fe9b4132514ea39eac873dda1bde49
dikesh9406 <93417755+dikesh9406@users.noreply.github.com>
2023-06-02 11:06:44 +0530

    Update server.js
    changed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+const http = require('http');
 import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
@@ -31,7 +32,7 @@ import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import authenticateUser from './middleware/auth.js';
 import health_data from "./routes/healthRoutes.js"
-import cors from "cors";
+const cors=require("cors");
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
@@ -46,11 +47,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(helmet());
-app.use(cors(
-  {
-    origin: "http://qts.iitkgp.ac.in",
-  }
-));
+app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 app.use(cookieParser());
@@ -78,7 +75,7 @@ app.use((req, res, next) => {
 
 app.use('api/v1/healthdata',health_data)
 
-
+const server=http.createServer(app);
 
 //////////////////////////
 // only when ready to deploy
@@ -93,7 +90,7 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () => {
+    server.listen(port, () => {
     
       console.log(`Server is listening on port ${port}...`);
     });
